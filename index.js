756800c49249fcfd4b69b964a7f55d03a70117b4
alviasad0 <alviasad10.com>
2023-10-18 11:42:02 +0600

    post single data using app.post

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 /* middleweres */
 app.use(express.json())
@@ -38,7 +38,60 @@ async function run() {
         app.post('/products', async (req, res) => {
             const product = req.body
             const result = await productCollection.insertOne(product)
+           
+            res.send(result);
+        })
+
+
+        /* get single data using name */
+        app.get("/products/:id", async (req, res) => {
+            const id = req.params.id
+            
+            const query = {
+                _id : new ObjectId(id)
+            }
+            const result = await productCollection.findOne(query)
             console.log(result);
+            res.send(result)
+            
+        })
+
+
+        /* update a single data */
+
+        app.put('/products/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = {
+                _id: new ObjectId(id)
+            }
+            const newProduct = req.body
+            const options = {
+                upsert: true,
+            }
+            const updatedProduct = {
+                $set: {
+                    image_url: newProduct.image_url,
+                    name: newProduct.name,
+                    brand_name: newProduct.brand_name,
+                    type: newProduct.type,
+                    details : newProduct.details,
+                    price: newProduct.price,
+                    short_description: newProduct.short_description,
+                    rating: newProduct.rating
+
+                }
+            }
+            const result = await productCollection.updateOne(filter , updatedProduct , options)
+            console.log(result);
+            res.send(result);
+        })
+
+
+
+        /* read data */
+        app.get('/products', async (req, res) => {
+            const result = await productCollection.find().toArray()
+            
             res.send(result);
         })
        
